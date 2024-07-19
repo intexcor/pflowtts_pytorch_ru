48e59a9ed033c4d5f13dad45dd367e0cff867c95
Ivan Shivalov <142020129+intexcor@users.noreply.github.com>
2024-07-19 12:58:31 +0300

    Update train.py

--- pflow/train.py
+++ pflow/train.py
@@ -62,7 +62,7 @@ def train(cfg: DictConfig) -> Tuple[Dict[str, Any], Dict[str, Any]]:
     logger: List[Logger] = utils.instantiate_loggers(cfg.get("logger"))
 
     log.info(f"Instantiating trainer <{cfg.trainer._target_}>")  # pylint: disable=protected-access
-    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger)
+    trainer: Trainer = hydra.utils.instantiate(cfg.trainer, callbacks=callbacks, logger=logger, strategy=DDPStrategy(find_unused_parameters=True))
 
     log.info("Instantiating transfer learning...")
     if cfg.get("transfer_ckpt_path") is not None:
